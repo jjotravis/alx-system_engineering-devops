ba673d04bdaa8610cfbd7f72af57c41268af839a
Travis <ondegotravis@gmail.com>
2024-03-25 20:04:34 +0300

    First line format

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     #         completed = t.get("title")
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print(
-        "Employee {} is done with tasks {}/{}:".format(
+        "Employee {} is done with tasks({}/{}):".format(
             user.get("name"), len(completed), len(todos)
         )
     )
