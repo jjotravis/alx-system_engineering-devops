d207ad9c3903704e9760c963135ede32f9fb811e
Travis <ondegotravis@gmail.com>
2024-03-25 20:31:19 +0300

    Quotes in task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,9 +2,9 @@
 """For a given employee ID
 returns information about his/her TODO list progress
 export data in the CSV format."""
+import csv
 import requests
 import sys
-import csv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
 
     with open(f'{user_id}.csv', 'w', newline="") as csv_file:
-        writer = csv.writer(csv_file)
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for t in todos:
             writer.writerow(
                 [user_id, user.get("username"),
